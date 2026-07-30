0fd05dbddf20c9e73ee253110671b20e99310846
udaysharma189 <udaysharma9027@gmail.com>
2026-07-30 11:25:19 +0530

    function overloading

---
+++ sizeof.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a;
+    cout << sizeof(a)<<endl;
+    char b;
+    cout << sizeof(b)<<endl;
+    float c;
+    cout << sizeof(c)<<endl;
+    double d;
+    cout << sizeof(d)<<endl;
+}
