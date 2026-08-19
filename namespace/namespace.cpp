7d125c1c893bde5ccc6c8b2d26aafa2eea910ecc
udaysharma189 <udaysharma9027@gmail.com>
2026-08-19 11:19:33 +0530

    method or namespace change

--- namespace/namespace.cpp
+++ namespace/namespace.cpp
@@ -11,23 +11,53 @@
 
 //     return 0;
 // }
+// #include <iostream>
+// using namespace std;
+
+// namespace first {
+//     int a = 10;
+// }
+
+// namespace second {
+//     double a = 10.5;
+// }
+
+// int main() {
+//     string a = "C++";
+
+//     cout << a << endl;
+//     cout << second::a << endl;
+//     cout << first::a << endl;
+
+//     return 0;
+// }
 #include <iostream>
 using namespace std;
 
-namespace first {
+namespace First {
     int a = 10;
+
+    void display() {
+        cout << "First namespace" << endl;
+        cout << "a = " << a << endl;
+    }
 }
 
-namespace second {
-    double a = 10.5;
+namespace Second {
+    int b = 20;
+
+    void show() {
+        cout << "Second namespace" << endl;
+        cout << "b = " << b << endl;
+    }
 }
 
 int main() {
-    string a = "C++";
+    cout << First::a << endl;
+    First::display();
 
-    cout << a << endl;
-    cout << second::a << endl;
-    cout << first::a << endl;
+    cout << Second::b << endl;
+    Second::show();
 
     return 0;
 }
