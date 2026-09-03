f98edce662e0fd67e7325c8ce1cf10455178db9c
udaysharma189 <udaysharma9027@gmail.com>
2026-09-03 16:05:18 +0530

    sum and multiple template

---
+++ variadicfunction/multipletemplate.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+template<class T1, class T2>
+void display(T1 a, T2 b){
+    cout<<a<<" "<<b<<endl;
+}
+int main(){
+    display(10, 20.5);
+    display(10.5, 20);
+    display("Uday", "Sharma");
+}
